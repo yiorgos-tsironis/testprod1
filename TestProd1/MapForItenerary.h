199eb8519a27474cede698b33b74a96ac958273a
George Tsironis <george@georges-mbp.lan>
2014-05-30 16:55:46 +1000

    Map feature added

---
+++ TestProd1/MapForItenerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.h
+//  TestProd1
+//
+//  Created by George Tsironis on 30/05/2014.
+//  Copyright (c) 2014 Yiorgos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItenerary : NSObject
+
+@end
